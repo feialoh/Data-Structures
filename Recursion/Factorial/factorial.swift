05ae115c1b8e95aeaa6919bc52430851dc1f847f
feialoh <feialohfrancis@gmail.com>
2022-11-13 12:35:17 +0530

    Initial Setup

---
+++ Recursion/Factorial/factorial.swift
@@ -0,0 +1,18 @@
+
+print("Hello world")
+// func factorial(int n) -> Int {
+//     if n == 0 {
+//         return 1
+//     }
+
+//     return n * factorial(n-1)
+// }
+
+// print("Enter the number: ")
+// let num = readLine()
+
+// guard  let number = int(num), number > 0 else {
+//     print("Invalid input")
+//     return
+// }
+// print("Factorial is \(factorial(number))")
