fd9b91df034b7daf22886696ec7a50c7c3ed6349
Feialoh Francis <feialohfrancis@gmail.com>
2023-04-11 11:23:17 +0530

    Palindrome faster without recursion

--- Recursion/004.Palindrome/Palindrome.swift
+++ Recursion/004.Palindrome/Palindrome.swift
@@ -30,11 +30,29 @@ class Palindrome {
     func isPalindrome(_ string: String) -> Bool {
         
         var palStr = string.lowercased()
-        palStr = String(palStr.filter { !" \n\t\r".contains($0) })
+        // palStr = String(palStr.filter { !" \n\t\r".contains($0) })
+        palStr = palStr.filter { $0.isNumber || $0.isLetter }.lowercased()
         if palStr.isEmpty {
             return true
         }
         return helper(0, palStr.count-1, palStr)
+    }
+
+    func isPalindrome2(_ s: String) -> Bool {
+        let filterdStr = s.filter { $0.isLetter || $0.isNumber }.lowercased()
+
+        if filterdStr.isEmpty { return true}
+        var startIndex = filterdStr.startIndex
+        var endIndex: String.Index = filterdStr.index(before: filterdStr.endIndex)
+
+        while (startIndex < endIndex) {
+            if filterdStr[startIndex] != filterdStr[endIndex] {
+                return false
+            }
+            startIndex = filterdStr.index(after: startIndex)
+            endIndex = filterdStr.index(before: endIndex)
+        }
+        return true
     }
 }
 
